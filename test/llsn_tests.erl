86694f3a7d5a910e56ce5f9e79a43b050c8d00e6
Taras Halturin <halturin@gmail.com>
2015-08-04 00:18:45 +0300

    sync

--- test/llsn_tests.erl
+++ test/llsn_tests.erl
@@ -47,9 +47,10 @@ llsn_encode_unsigned_numbers_test() ->
 llsn_random_signed_NUMBER(0) ->
     ok;
 llsn_random_signed_NUMBER(N) ->
-    Num = llsn_gen:random_signed_number(),
+    Num         = llsn_gen:random_signed_number(),
     {BinNum, _} = llsn:encode_NUMBER(Num),
     {DecNum, _} = llsn:decode_NUMBER(BinNum),
+
     ?assert(Num =:= DecNum),
     llsn_random_signed_NUMBER(N-1).
 
@@ -61,9 +62,10 @@ llsn_random_unsigned_NUMBER(0) ->
     ok;
 
 llsn_random_unsigned_NUMBER(N) ->
-    Num = llsn_gen:random_unsigned_number(),
+    Num         = llsn_gen:random_unsigned_number(),
     {BinNum, _} = llsn:encode_UNUMBER(Num),
     {DecNum, _} = llsn:decode_UNUMBER(BinNum),
+
     ?assert(Num =:= DecNum),
     llsn_random_unsigned_NUMBER(N-1).
 
@@ -77,12 +79,12 @@ llsn_1K_random_FLOAT_test() ->
     ok.
 
 llsn_encodeComplexStruct_test() ->
-    Value = get_exampleMainValue(),
+    Value       = get_exampleMainValue(),
     Declaration = get_exampleMainDeclaration(),
-    ValueBin = get_exampleMainValueEncoded(),
-    Bin = llsn:encode(Value,Declaration,4),
-    ?assert(Bin =:= ValueBin),
-    ok.
+    ValueBin    = get_exampleMainValueEncoded(),
+    Bin         = llsn:encode(Value,Declaration,4),
+
+    ?assert(Bin =:= ValueBin).
 
 llsn_encodeEtalonEncodeDecodeEtalon_Threshold_test() ->
     Etalon      = get_exampleMainValue(),
@@ -92,14 +94,13 @@ llsn_encodeEtalonEncodeDecodeEtalon_Threshold_test() ->
 
     Etalon15    = element(15, Etalon),
     Etalon15_1  = Etalon15#llsn_file{origin = null},
-    EtalonFixed  = setelement(15, Etalon, Etalon15_1),
+    EtalonFixed = setelement(15, Etalon, Etalon15_1),
 
     Value15     = element(15, Value),
     Value15_1   = Value15#llsn_file{origin = null},
     ValueFixed  = setelement(15, Value, Value15_1),
 
-    ?assert(EtalonFixed =:= ValueFixed),
-    ok.
+    ?assert(EtalonFixed =:= ValueFixed).
 
 llsn_encodeEtalonEncodeDecodeEtalon_NoThreshold_test() ->
     Etalon      = get_exampleMainValue(),
@@ -109,7 +110,7 @@ llsn_encodeEtalonEncodeDecodeEtalon_NoThreshold_test() ->
 
     Etalon15    = element(15, Etalon),
     Etalon15_1  = Etalon15#llsn_file{origin = null},
-    EtalonFixed  = setelement(15, Etalon, Etalon15_1),
+    EtalonFixed = setelement(15, Etalon, Etalon15_1),
 
     Value15     = element(15, Value),
     Value15_1   = Value15#llsn_file{origin = null},
@@ -130,50 +131,49 @@ llsn_1K_encodeComplexStruct_test() ->
 
 
 llsn_decodeComplexStruct_test() ->
-    ValueBin = get_exampleMainValueEncoded(),
-    Value = llsn:decode(ValueBin),
+    ValueBin    = get_exampleMainValueEncoded(),
+    Value       = llsn:decode(ValueBin),
     % we have to remove 'origin' from 'llsn_file'
-    F0 = element(15, Value),
-    F1 = F0#llsn_file{origin = null},
-    Value1 = setelement(15, Value, F1),
+    F0          = element(15, Value),
+    F1          = F0#llsn_file{origin = null},
+    Value1      = setelement(15, Value, F1),
 
-    MainValue = get_exampleMainValue(),
-    FF0 = element(15, MainValue),
-    FF1 = FF0#llsn_file{origin = null},
-    MainValue1 = setelement(15, MainValue, FF1),
+    MainValue   = get_exampleMainValue(),
+    FF0         = element(15, MainValue),
+    FF1         = FF0#llsn_file{origin = null},
+    MainValue1  = setelement(15, MainValue, FF1),
 
     ?assert(Value1 =:= MainValue1).
 
 
 llsn_decodeComplexStructSlowStream_test() ->
-    ValueBin = get_exampleMainValueEncoded(),
-    Value = slow_stream(ValueBin, null),
+    ValueBin    = get_exampleMainValueEncoded(),
+    Value       = slow_stream(ValueBin, null),
     % we have to remove 'origin' from 'llsn_file'
-    F0 = element(15, Value),
-    io:format("~p ~n", [F0]),
-    F1 = F0#llsn_file{origin = null},
-    Value1 = setelement(15, Value, F1),
+    F0          = element(15, Value),
+    F1          = F0#llsn_file{origin = null},
+    Value1      = setelement(15, Value, F1),
 
-    MainValue = get_exampleMainValue(),
-    FF0 = element(15, MainValue),
-    FF1 = FF0#llsn_file{origin = null},
-    MainValue1 = setelement(15, MainValue, FF1),
+    MainValue   = get_exampleMainValue(),
+    FF0         = element(15, MainValue),
+    FF1         = FF0#llsn_file{origin = null},
+    MainValue1  = setelement(15, MainValue, FF1),
 
     ?assert(Value1 =:= MainValue1).
 
 llsn_decodeComplexStructSlowStreamNoThreshold_test() ->
-    Value0 = get_exampleMainValue(),
+    Value0      = get_exampleMainValue(),
     Declaration = get_exampleMainDeclaration(),
     ValueBin    = llsn:encode(Value0,Declaration),
-    Value1 = slow_stream(ValueBin, null),
+    Value1      = slow_stream(ValueBin, null),
     % we have to remove 'origin' from 'llsn_file'
-    F0 = element(15, Value0),
-    F0_1 = F0#llsn_file{origin = null},
-    Value0_1 = setelement(15, Value0, F0_1),
+    F0          = element(15, Value0),
+    F0_1        = F0#llsn_file{origin = null},
+    Value0_1    = setelement(15, Value0, F0_1),
 
-    FF0 = element(15, Value1),
-    FF1 = FF0#llsn_file{origin = null},
-    Value1_1 = setelement(15, Value1, FF1),
+    FF0         = element(15, Value1),
+    FF1         = FF0#llsn_file{origin = null},
+    Value1_1    = setelement(15, Value1, FF1),
 
     ?assert(Value0_1 =:= Value1_1).
 
@@ -187,13 +187,12 @@ llsn_decodeEtalonDecodeEncodeEtalon_test() ->
 
 
 receive_frame(Bin) ->
-
     receive
-        {frame, N, _Size, Frame, UserData } ->
+        {frame, _N, _Size, Frame, UserData } ->
             ?assert(UserData =:= [userdata]),
             receive_frame(<<Bin/binary,Frame/binary>>);
 
-        {done, N, _Size, Frame, UserData} ->
+        {done, _N, _Size, Frame, UserData} ->
             ?assert(UserData =:= [userdata]),
             <<Bin/binary,Frame/binary>>
 
@@ -201,12 +200,13 @@ receive_frame(Bin) ->
     end.
 
 llsn_encodeComplexStruct_with_Framing_test() ->
-    Value = get_exampleMainValue(),
+    Value       = get_exampleMainValue(),
     Declaration = get_exampleMainDeclaration(),
-    ok    = llsn:encode(Value, Declaration, self(), 50, [userdata]),
-    ValueBin = receive_frame(<<>>),
-    ValueBinNoFrame   =  llsn:encode(Value, Declaration),
-    ?assert(ValueBin =:= ValueBinNoFrame).
+    ok          = llsn:encode(Value, Declaration, self(), 50, [userdata]),
+    Bin         = receive_frame(<<>>),
+    BinNoFrame  =  llsn:encode(Value, Declaration),
+
+    ?assert(Bin =:= BinNoFrame).
 
 
 
@@ -233,13 +233,13 @@ get_exampleMainValueEncoded() ->
     <<16, 4, 19, 1, 33, 254, 12, 131, 120, 9, 3, 7, 1, 0, 1, 2, 6, 224, 47, 239, 220, 3, 128, 146, 6, 7, 223, 71, 195, 137, 234, 96, 0, 249, 8, 2, 1, 0, 247, 9, 5, 8, 2, 1, 0, 247, 64, 0, 64, 0, 64, 0, 64, 0, 246, 10, 4, 32, 8, 2, 1, 23, 8, 2, 1, 24, 247, 0, 25, 0, 22, 2, 1, 21, 247, 64, 26, 10, 10, 223, 10, 5, 208, 8, 2, 1, 27, 247, 64, 28, 64, 4, 160, 64, 29, 0, 30, 2, 1, 31, 247, 4, 13, 5, 75, 12, 108, 108, 115, 110, 116, 101, 115, 116, 102, 105, 108, 101, 250, 9, 34, 1, 191, 192, 65, 63, 128, 64, 223, 224, 0, 160, 1, 159, 255, 192, 32, 0, 239, 240, 0, 0, 208, 0, 1, 207, 255, 255, 224, 16, 0, 0, 247, 248, 0, 0, 0, 232, 0, 0, 1, 231, 255, 255, 255, 240, 8, 0, 0, 0, 251, 252, 0, 0, 0, 0, 244, 0, 0, 0, 1, 243, 255, 255, 255, 255, 248, 4, 0, 0, 0, 0, 253, 254, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 1, 249, 255, 255, 255, 255, 255, 252, 2, 0, 0, 0, 0, 0, 254, 255, 0, 0, 0, 0, 0, 0, 253, 0, 0, 0, 0, 0, 1, 252, 255, 255, 255, 255, 255, 255, 254, 1, 0, 0, 0, 0, 0, 0, 255, 255, 128, 0, 0, 0, 0, 0, 0, 254, 128, 0, 0, 0, 0, 0, 1, 254, 127, 255, 255, 255, 255, 255, 255, 255, 0, 128, 0, 0, 0, 0, 0, 0, 255, 128, 0, 0, 0, 0, 0, 0, 1, 255, 127, 255, 255, 255, 255, 255, 255, 255, 9, 17, 12, 127, 128, 128, 191, 255, 192, 64, 0, 223, 255, 255, 224, 32, 0, 0, 239, 255, 255, 255, 240, 16, 0, 0, 0, 247, 255, 255, 255, 255, 248, 8, 0, 0, 0, 0, 251, 255, 255, 255, 255, 255, 252, 4, 0, 0, 0, 0, 0, 253, 255, 255, 255, 255, 255, 255, 254, 2, 0, 0, 0, 0, 0, 0, 254, 255, 255, 255, 255, 255, 255, 255, 255, 1, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 10, 5, 168, 10, 3, 0, 12, 131, 120, 131, 120, 131, 120, 4, 224, 131, 120, 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 46, 32, 228, 189, 160, 229, 165, 189, 228, 184, 150, 231, 149, 140, 46, 32, 217, 133, 216, 177, 216, 173, 216, 168, 216, 167, 32, 216, 168, 216, 167, 217, 132, 216, 185, 216, 167, 217, 132, 217, 133, 46, 32, 227, 129, 147, 227, 130, 147, 227, 129, 171, 227, 129, 161, 227, 129, 175, 228, 184, 150, 231, 149, 140, 46, 32, 206, 147, 206, 181, 206, 185, 206, 172, 32, 206, 163, 206, 191, 207, 133, 32, 206, 154, 207, 140, 207, 131, 206, 188, 206, 181, 46, 32, 215, 148, 215, 162, 215, 156, 215, 144, 32, 215, 149, 215, 149, 215, 162, 215, 156, 215, 152, 46, 32, 208, 159, 209, 128, 208, 184, 208, 178, 208, 181, 209, 130, 32, 208, 156, 208, 184, 209, 128, 46, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 7, 7, 7, 84, 104, 105, 115, 32, 105, 115, 32, 100, 101, 109, 111, 32, 102, 105, 108, 101, 46, 32, 84, 104, 105, 115, 32, 105, 115, 32, 100, 101, 109, 111, 32, 102, 105, 108, 101, 46, 32, 84, 104, 105, 115, 32, 105, 115, 32, 100, 101, 109, 111, 32, 102, 105, 108, 101, 46, 32, 84, 104, 105, 115, 32, 105, 115, 32, 100, 101, 109, 111, 32, 102, 105, 108, 101, 46>>.
 
 get_exampleMainValue() ->
-    TmpFile = "/tmp/llsntestfile",
+    TmpFile     = "/tmp/llsntestfile",
     ContentFile = "This is demo file. This is demo file. This is demo file. This is demo file.",
     file:write_file(TmpFile, ContentFile),
 
-   {33, null, 888,                      %% Field1, Field2, Field3
-    [true, false, true],                %% Field4
-    3.141596,                           %% Field5
+   {33, null, 888,                                      %% Field1, Field2, Field3
+    [true, false, true],                                %% Field4
+    3.141596,                                           %% Field5
     "Hello World. 你好世界. مرحبا بالعالم. こんにちは世界. Γειά Σου Κόσμε. העלא וועלט. Привет Мир.",
     {{2015, 4, 15},{16, 56, 39, 678},{0,0}},            %% Field7 = 4 Apr, 2015 16:56:39.678 +0000
     null,                                               %% Field8
@@ -253,9 +253,8 @@ get_exampleMainValue() ->
     [null, null,                                        %%
         [null, null, {27, null} , null, {28, null}],    %%
      null, null, null, null, null,                      %%
-        [null, {29, null} ,null, {30, {31, null}}],      %%
-     null],                                                  %% Field13
-
+        [null, {29, null} ,null, {30, {31, null}}],     %%
+     null],                                             %% Field13
     <<8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 7, 7, 7>>,          %% Field14
     ?LLSN_FILE(TmpFile),             %% Field15
     null,                                               %% Field16
